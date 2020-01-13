ecd2af9465bd414fc58431ccb728bbab98ee82a4
Christopher Greene <greenecr@bc.edu>
2020-01-13 18:34:21 -0500

    Week One

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Christopher Greene on 1/13/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sayItPressed(_ sender: UIButton) {
+        messageLabel.text = "I'm rockin' swift"
+    }
+    
+}
